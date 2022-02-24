24d98201524a41af321fbdcf3a6684c781d1da56
1uy31 <snow.tuyet.tran.31@gmail.com>
2022-02-24 19:16:36 +0200

    Tests grouping

--- tests/models/test_category_connector.py
+++ tests/models/test_category_connector.py
@@ -6,32 +6,33 @@ import pytest
 ###
 
 
-def test_delete_category_by_name_happy(app_with_fresh_database):
-    with app_with_fresh_database.app_context():
-        from core.models import Category, CategoryConnector
+class TestDeleteCategory:
+    def test_delete_category_by_name_happy(self, app_with_fresh_database):
+        with app_with_fresh_database.app_context():
+            from core.models import Category, CategoryConnector
 
-        database = app_with_fresh_database.extensions["migrate"].db
+            database = app_with_fresh_database.extensions["migrate"].db
 
-        connector = CategoryConnector()
+            connector = CategoryConnector()
 
-        # Setup
-        category = Category(name="Test_Category")
-        database.session.add(category)
-        database.session.commit()
-        assert category.id is not None
+            # Setup
+            category = Category(name="Test_Category")
+            database.session.add(category)
+            database.session.commit()
+            assert category.id is not None
 
-        # Test:
-        connector.delete_category_by_name("Test_Category")
-        retrieve_category = database.session.get(Category, category.id)
-        assert retrieve_category is None
-
-
-def test_delete_category_by_name_raises_not_exist(app_with_fresh_database):
-    with app_with_fresh_database.app_context():
-        from core.models import CategoryConnector
-
-        connector = CategoryConnector()
-
-        with pytest.raises(Exception) as exc:
+            # Test:
             connector.delete_category_by_name("Test_Category")
-        assert str(exc.value) == "There is no Category with name Test_Category."
+            retrieve_category = database.session.get(Category, category.id)
+            assert retrieve_category is None
+
+
+    def test_delete_category_by_name_raises_not_exist(self, app_with_fresh_database):
+        with app_with_fresh_database.app_context():
+            from core.models import CategoryConnector
+
+            connector = CategoryConnector()
+
+            with pytest.raises(Exception) as exc:
+                connector.delete_category_by_name("Test_Category")
+            assert str(exc.value) == "There is no Category with name Test_Category."
